c3a79ab6fe2b923a3309db1f8eb8002d5c227aed
Kamyar <kamsignal21@gmail.com>
2019-05-08 04:49:58 +0000

    make

--- application.py
+++ application.py
@@ -10,6 +10,20 @@ from passlib.apps import custom_app_context as pwd_context
 
 from helpers import apology, login_required, lookup, usd
 
+
+# added the below as part of Heroku post on Medium
+import urllib.parse
+import psycopg2
+urllib.parse.uses_netloc.append("postgres")
+url = urllib.parse.urlparse(os.environ["postgres://jvqrtbugnndzjg:681d3846f6e0f13ba84155d2ad50b4b56c4d5b2ffc23e58137c4383c01da4661@ec2-174-129-208-118.compute-1.amazonaws.com:5432/d9krdf0f2pbkjs"])
+conn = psycopg2.connect(
+ database=url.path[1:],
+ user=url.username,
+ password=url.password,
+ host=url.hostname,
+ port=url.port
+)
+# end
 # Configure application
 app = Flask(__name__)
 
@@ -40,7 +54,7 @@ Session(app)
 
 # Configure CS50 Library to use SQLite database
 # db = SQL("sqlite:///finance.db")
-db = SQL("postgres://jvqrtbugnndzjg:681d3846f6e0f13ba84155d2ad50b4b56c4d5b2ffc23e58137c4383c01da4661@ec2-174-129-208-118.compute-1.amazonaws.com:5432/d9krdf0f2pbkjs")
+db = SQL(os.environ["postgres://jvqrtbugnndzjg:681d3846f6e0f13ba84155d2ad50b4b56c4d5b2ffc23e58137c4383c01da4661@ec2-174-129-208-118.compute-1.amazonaws.com:5432/d9krdf0f2pbkjs"])
 
 # GET: you access the route via url /password
 # POST: you submit a form to /password and doesnt show
