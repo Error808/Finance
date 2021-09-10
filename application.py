45cd702e813e774961e6caf902bdea35fc47fe8f
Kamyar <kamsignal21@gmail.com>
2021-09-10 15:52:01 -0700

    fixed session

--- application.py
+++ application.py
@@ -34,7 +34,7 @@ app.jinja_env.filters["usd"] = usd
 
 # app.config["SESSION_FILE_DIR"] = mkdtemp()
 # app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "sqlalchemy"
+# app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
