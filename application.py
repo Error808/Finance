8cb05b63e0d3933f15f5215c2a9c989a9e4047e2
Kamyar <kamsignal21@gmail.com>
2021-09-10 16:26:44 -0700

    fixed ENV var

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-from boto.s3.connection import S3Connection
+from boto3.s3.connection import S3Connection
 s3 = S3Connection(os.environ['API_KEY'], os.environ['DATABASE_URL'])
 
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
